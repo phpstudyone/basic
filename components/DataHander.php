1f6ba35313e196a2b0922207ec742220136a3384
Jasong Zhang <845830229@qq.com>
2017-01-01 01:31:18 +0800

    根据表名获取建表语句的方法

--- components/DataHander.php
+++ components/DataHander.php
@@ -7,18 +7,32 @@ namespace app\components;
 
 use yii;
 class DataHander{
-
+    
     /**
      * 获取数据表所有的表
+     * @return array|mixed
      */
     public static function getAllTables(){
         $allTables = Redis::getCache('allTables');
         $isOver = Redis::getCache('isOver');
-        if(!$allTables && $isOver == 0){
+        if(!$allTables){
             $sql = "show tables";
             $allTables = Yii::$app->db->createCommand($sql)->queryAll();
             Redis::setCache('allTables',$allTables);
-            return $allTables;
+        }
+        return $allTables;
+    }
+
+    /**
+     * 根据表名获取建表语句
+     * @param $tableName
+     * @return bool|mixed
+     */
+    public static function getCreateTableSql($tableName){
+        $sql = "SHOW CREATE TABLE " . $tableName;
+        $createSql = Yii::$app->db->createCommand($sql)->queryOne();
+        if($createSql){
+            return end($createSql);
         }else return false;
     }
 }
