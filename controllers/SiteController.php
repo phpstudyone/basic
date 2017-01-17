6780695c8f6f6558ed850c01ed1a236fa770fd84
Jason Zhang <jaszhang@xogrp.com>
2017-01-17 17:52:17 +0800

    no message

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -41,6 +41,74 @@ class SiteController extends Controller
         ];
     }
 
+    /**
+     * 更具用户id生产 tbl_arawrds 表数据
+     *
+     * user_id, first_name, last_name, business_name, email, user_type, subscription, status, published_count, city, state, country,
+    accessed_award_site, copied_bade_info, tweeted, facebook_shared, copied_blog_header, copied_larger_badge, copied_certificate,
+    download_small_badge, download_large_badge, download_certificate, download_blog_header, download_small_badge_count,
+    download_large_badge_count, download_blog_header_count, download_certificate_count, year, accepted_online, accepted_partial, accepted_print
+     *
+     */
+    public function actionCreateSql(){
+        $emails = ['gzp1@twobrightlights.com','gzp2@twobrightlights.com','gzp3@twobrightlights.com'];
+        foreach ($emails as $email){
+            //查询id, email,first_name,last_name,organization_name,user_type,user_status as statuscity,subscription_name
+            $sql = "select
+  u.id as user_id, u.email, up.first_name, up.last_name, up.organization_name, ut.user_type, u.user_status as status,ua.city, s.subscription_name
+from users u
+  left join user_profiles up on u.id=up.user_id
+  left join usertypes ut on u.usertype_id=ut.id
+  left join user_addresses ua on u.id=ua.user_id
+  left join user_subscriptions us on u.id=us.user_id
+  left join subscriptions s on us.subscription_id=s.id
+where u.email ='".$email."' limit 1;";
+            $result = Yii::$app->db->createCommand($sql)->queryOne();
+            if($result['user_id']){
+                $insertData = [
+                    'user_id' => $result['user_id'],
+                    'first_name' => $result['first_name'],
+                    'last_name' => $result['last_name'],
+                    'business_name' => $result['organization_name'],
+                    'email' => $result['email'],
+                    'user_type' => $result['user_type'],
+                    'subscription' => $result['subscription_name'],
+                    'status' => $result['status'],
+                    'published_count' => null,
+                    'city' => $result['city'],
+                    'state' => null,
+                    'country' => null,
+                    'accessed_award_site' => null,
+                    'copied_bade_info' => null,
+                    'tweeted' => null,
+                    'facebook_shared' => null,
+                    'copied_blog_header' => null,
+                    'copied_larger_badge' => null,
+                    'copied_certificate' => null,
+                    'download_small_badge' => null,
+                    'download_large_badge' => null,
+                    'download_certificate' => null,
+                    'download_blog_header' => null,
+                    'download_small_badge_count' => null,
+                    'download_large_badge_count' => null,
+                    'download_blog_header_count' => null,
+                    'download_certificate_count' => null,
+                    'year' => '2017',
+                    'accepted_online' => null,
+                    'accepted_partial' => null,
+                    'accepted_print' => null
+                ];
+                $key = implode(array_keys($insertData),',');
+                $value = '"' . implode(array_values($insertData),'","') . '"';
+
+
+                echo "INSERT INTO twobrightlights.tbl_awards(" . $key . ")values(" . $value . ");" . "<br />";
+
+            }
+        }
+
+    }
+
     public function actions()
     {
         return [
