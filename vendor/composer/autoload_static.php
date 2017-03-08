8c6e0d341eb0c03135c5e1e006076963885a2960
Jason Zhang <jaszhang@xogrp.com>
2017-03-08 19:09:31 +0800

    发送邮件

--- vendor/composer/autoload_static.php
+++ vendor/composer/autoload_static.php
@@ -113,12 +113,24 @@ class ComposerStaticInit0fb7274d6961f08116ce84af67e50509
         ),
     );
 
+    public static $classMap = array (
+        'EasyPeasyICS' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/EasyPeasyICS.php',
+        'PHPMailer' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+        'PHPMailerOAuth' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauth.php',
+        'PHPMailerOAuthGoogle' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmaileroauthgoogle.php',
+        'POP3' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.pop3.php',
+        'SMTP' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.smtp.php',
+        'ntlm_sasl_client_class' => __DIR__ . '/..' . '/phpmailer/phpmailer/extras/ntlm_sasl_client.php',
+        'phpmailerException' => __DIR__ . '/..' . '/phpmailer/phpmailer/class.phpmailer.php',
+    );
+
     public static function getInitializer(ClassLoader $loader)
     {
         return \Closure::bind(function () use ($loader) {
             $loader->prefixLengthsPsr4 = ComposerStaticInit0fb7274d6961f08116ce84af67e50509::$prefixLengthsPsr4;
             $loader->prefixDirsPsr4 = ComposerStaticInit0fb7274d6961f08116ce84af67e50509::$prefixDirsPsr4;
             $loader->prefixesPsr0 = ComposerStaticInit0fb7274d6961f08116ce84af67e50509::$prefixesPsr0;
+            $loader->classMap = ComposerStaticInit0fb7274d6961f08116ce84af67e50509::$classMap;
 
         }, null, ClassLoader::class);
     }
